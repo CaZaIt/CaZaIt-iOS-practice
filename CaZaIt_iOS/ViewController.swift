0021563134325b7f9bbd729f034e37d478e2a757
hsseor <hsseor@naver.com>
2023-01-27 16:51:28 +0900

    initial setting

---
+++ CaZaIt_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaZaIt_iOS
+//
+//  Created by 홍서린 on 2023/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
